1bef24d69612104e2512693d1eec1c811b09330a
longtrinh2666 <longtrinh.2666@gmail.com>
2026-02-13 05:20:07 -0800

    Add files via upload

---
+++ recovery/recovery.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/bin/bash --rcfile <(echo "export PS1=\"\[\e[1;34m\]recovery-mod\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\\$ \" ; export PATH=\"\$PATH:/home/container/usr/bin\"; cd recovery; clear") -i
