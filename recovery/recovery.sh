3aba2d81cbb43067b813157f3d4a5ed0930c0159
longtrinh2666 <longtrinh.2666@gmail.com>
2026-02-13 21:34:39 -0800

    Delete recovery directory

--- recovery/recovery.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/bin/bash --rcfile <(echo "export PS1=\"\[\e[1;34m\]recovery-mod\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\\$ \" ; export PATH=\"\$PATH:/home/container/usr/bin\"; cd recovery; clear") -i
